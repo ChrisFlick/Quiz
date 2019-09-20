55a68168da749e5d0ab2ca6694d181ff23f4cd3e
Chris Flick <chrisflick@gmail.com>
2019-09-20 14:29:32 -0700

    Initialized variables in main.js

---
+++ Assets/js/main.js
@@ -0,0 +1,18 @@
+document.querySelector("#question");
+document.querySelectorAll("button");
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    ///etc.
+  ];
+
+  var time = question.length * 15;
