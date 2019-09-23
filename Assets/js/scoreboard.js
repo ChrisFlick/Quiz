a267953bd7c4b2237565e945ffc83ed5efb00bf3
Chris Flick <chrisflick@gmail.com>
2019-09-23 13:57:12 -0700

    Makeing sure github is up to date

--- Assets/js/scoreboard.js
+++ Assets/js/scoreboard.js
@@ -4,5 +4,6 @@ var scores = JSON.parse(localStorage.getItem("scoreboard"))
 scores.sort()
 
 for (let i = scores.length - 1; i >= 0; i--) {
-    scoreboard.innerHTML += scores[i][1] + ": " + scores[i][0] + " seconds<br>"
+    let score = scoreboard.appendChild(document.createElement('li'));
+    score.innerHTML += scores[i][1] + ": " + scores[i][0] + " seconds<br>"
 }
